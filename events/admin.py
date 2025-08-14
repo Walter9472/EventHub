0b275c4d930a737d677e436b152dbdfefd56f2b7
Walter9472 <Walter.Mtanda@Student.HTW-Berlin.de>
2025-08-14 03:33:42 +0200

    feat: add admin interfaces for Event and Ticket models with custom display and search options

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import Event,Ticket
+
+
 # Register your models here.
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("title", "location", "start_date", "end_date","price", "category","owner","capacity")
+    search_fields = ("title","owner")
+    list_filter = ( "location", "start_date", "end_date", "category","owner")
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ("buyer", "event","seat", "purchase_date","status")
+    search_fields = ("buyer", "event","seat")
+    list_filter = ("buyer", "event","seat", "purchase_date","status")
